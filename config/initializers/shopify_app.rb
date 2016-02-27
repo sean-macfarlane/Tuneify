568ae59f9c3e9a92210dbbda8d5150d398ad653c
sean-macfarlane <sean.g.macfarlane@gmail.com>
2016-02-27 18:04:21 -0500

    change scopes

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,6 +2,6 @@ ShopifyApp.configure do |config|
   config.api_key = "6638f129de6d5fc5b4ce3009ae5136fc"
   config.secret = "84587a678a4a9b626727d40d9cc70c51"
   config.redirect_uri = "http://tuneify-shop.herokuapp.com/auth/shopify/callback"
-  config.scope = "read_orders, read_products"
+  config.scope = "read_orders, read_products, write_products, write_orders"
   config.embedded_app = true
 end
