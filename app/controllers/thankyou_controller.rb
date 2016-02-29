2889f0c9999936534e5ffe1ba3d0cc735cb6e919
sean-macfarlane <sean.g.macfarlane@gmail.com>
2016-02-29 04:06:01 -0500

    add checkout

--- app/controllers/thankyou_controller.rb
+++ app/controllers/thankyou_controller.rb
@@ -2,16 +2,8 @@ class ThankyouController < AuthenticatedController
 
 	def index
 		if session[:cart] 
-			@cart = session[:cart]
-		else
-			redirect_to root_path
+			session.delete(:cart)
 		end
-	end
-	
-	def download
-	@product = Product.find(params[:id])
-	
-	  send_file @product.audio.url, filename: @product.audio_file_name, type: @product.audio_content_type, disposition: 'attachment'
 	end
 end
 	
