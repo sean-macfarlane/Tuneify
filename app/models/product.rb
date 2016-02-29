e7f4e8e51bb1c2bcd1aa0c9245020ee72046e1b5
sean-macfarlane <sean.g.macfarlane@gmail.com>
2016-02-28 20:08:26 -0500

    changed products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ActiveRecord::Base
 	validates_presence_of :name
+	
+	has_attached_file :audio  ,
+                    :url => "/assets/:class/:id/:attachment/:style.:extension",
+                    :path => ":rails_root/public/assets/:class/:id/:attachment/:style.:extension"
+
+validates_attachment_content_type :audio, :content_type => [ 'audio/mpeg', 'audio/m4a', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
+
 end
