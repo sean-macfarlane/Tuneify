68ccc7c8de7bb46345de3043eafcc59528d601ab
sean-macfarlane <sean.g.macfarlane@gmail.com>
2016-02-28 22:28:53 -0500

    add images

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,9 @@ class Product < ActiveRecord::Base
 
 	belongs_to :category
 	validates_presence_of :name
-	
+	has_attached_file :image
+validates_attachment :image, presence: true,
+                     size: { in: 0..10000.kilobytes }
 	has_attached_file :audio  
 
 validates_attachment_content_type :audio, :content_type => [ 'audio/mpeg', 'audio/m4a', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
