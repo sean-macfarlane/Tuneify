49777709ced91310322a4ce98bb53131966756a2
sean-macfarlane <sean.g.macfarlane@gmail.com>
2016-02-28 21:43:28 -0500

    add amazon s3

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,7 @@
 class Product < ActiveRecord::Base
 	validates_presence_of :name
 	
-	has_attached_file :audio  ,
-                    :url => "/assets/:class/:id/:attachment/:style.:extension",
-                    :path => ":rails_root/public/assets/:class/:id/:attachment/:style.:extension"
+	has_attached_file :audio  
 
 validates_attachment_content_type :audio, :content_type => [ 'audio/mpeg', 'audio/m4a', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
 
