bea2f6949aca104a87c807bc2d6b46d6fd7c6eec
sean-macfarlane <sean.g.macfarlane@gmail.com>
2016-03-12 21:15:46 -0500

    change shopify creds

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,6 +1,6 @@
 ShopifyApp.configure do |config|
-  config.api_key = "6638f129de6d5fc5b4ce3009ae5136fc"
-  config.secret = "84587a678a4a9b626727d40d9cc70c51"
+  config.api_key = "2cccde181d6679e6ec5eb4c6c9afc718"
+  config.secret = "19813a79a4bd9958c9f80a390509de71"
   config.redirect_uri = "http://tuneify-shop.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products, write_products, write_orders"
   config.embedded_app = true
